09cee3c8cc2dea0929c708f1938bf306a839ceb3
Alin Bodea <cinewva@gmail.com>
2024-06-01 14:39:18 +0300

    translated to english and added Float.parseFloat to accept floating point numbers

--- src/com/itfactory/Main.java
+++ src/com/itfactory/Main.java
@@ -1,2 +1,33 @@
-package com.itfactory;public class Main {
+package com.itfactory;
+
+import java.util.Scanner;
+
+
+/*
+Problem 5.
+Read two numbers from the keyboard and display the multiplication of the two numbers and the division (with a comma) of the two numbers.
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        //read the first number
+        System.out.print("enter one number: ");
+        float nr1 = Float.parseFloat(scanner.nextLine());
+
+        // ask the user for the second number
+        System.out.print("enter another number: ");
+        float nr2 = Float.parseFloat(scanner.nextLine());
+
+       // show result of multiply and divide
+        System.out.println("the result of the multiplication is: " + (int)(nr1 * nr2));
+        System.out.println("the result of the division is: " + nr1 / nr2);
+
+
+    }
+
 }
